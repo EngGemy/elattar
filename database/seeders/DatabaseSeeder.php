bf437234bbe4cd6c00a07c5af9780810065833f9
engmgemy <engmgemy@gmail.com>
2026-07-14 15:25:15 +0300

    catlouge of this

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,9 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call([AttarSeeder::class]);
+        $this->call([
+            AttarSeeder::class,
+            ExpertCatalogSeeder::class,
+        ]);
     }
 }
